d24bbb5f3daf9a7902562464ca4ffb84714be90e
Anjor-Srivastava <81854563+Anjor-Srivastava@users.noreply.github.com>
2022-06-08 13:43:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {}
+
+function setup() {
+    var canvas = createCanvas(400, 400);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function snap() {
+    save("musacheFilter.png")
+}

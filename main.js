a1e946f23d6dfc1570590ca1deeefe71a864d711
Anjor-Srivastava <81854563+Anjor-Srivastava@users.noreply.github.com>
2022-06-14 19:56:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
-function preload() {}
+var noseX = 0;
+var noseY = 0;
+
+function preload() {
+    img = loadImage("mustache.png");
+}
 
 function setup() {
     var canvas = createCanvas(400, 400);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(400, 400);
+    video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPose);
+}
+
+function gotPose(results) {
+    if(results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X = " + noseX + ", NoseY = " + noseY);
+    }
+}
+
+function modelLoaded() {
+    console.log("AI Loaded");
 }
 
 function draw() {
+    image(video, 0, 0, 400, 400);
+    //image(img, noseX, noseY, 150, 41)
 }
 
 function snap() {
